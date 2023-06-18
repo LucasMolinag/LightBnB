e820d0203c2abcc680ec431d76e0d42b0f978d1f
Lucas <molinag.lucas@gmail.com>
2023-06-18 16:38:24 -0700

    Implement addProperty

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -64,10 +64,10 @@ const addUser = function (user) {
   .query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;`, [user.name, user.email, user.password])
   .then((result) => {
     const insertedUser = result.rows[0];
-    return(insertedUser);
+    return insertedUser;
   })
     .catch((err) => {
-    return(err.message);
+    return err.message;
   });
 };
 
@@ -91,7 +91,7 @@ const getAllReservations = function (guest_id, limit = 10) {
   return pool.query(query, [guest_id])
   .then((result) => {
     const reservations = result.rows;
-    return(reservations);
+    return reservations;
   })
   .catch((err) => {
     console.log(err);
@@ -177,10 +177,45 @@ const getAllProperties = function (options, limit = 10) {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  return pool
+  .query(`INSERT INTO properties (
+    owner_id,
+    title,
+    description,
+    thumbnail_photo_url,
+    cover_photo_url,
+    cost_per_night,
+    street, city,
+    province,
+    post_code,
+    country,
+    parking_spaces,
+    number_of_bathrooms, 
+    number_of_bedrooms) 
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) 
+    RETURNING *;`, 
+    [property.owner_id, 
+    property.title, 
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night * 100,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    property.country,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms])
+
+  .then((result) => {
+    const addedProperty = result.rows[0];
+    return addedProperty;
+  })
+    .catch((err) => {
+    return err.message;
+  });
 };
 
 module.exports = {
