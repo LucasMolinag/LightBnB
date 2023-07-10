177dc148c4dd3b1b898d4146378576787bcab3c1
Lucas <molinag.lucas@gmail.com>
2023-07-09 22:51:27 -0700

    Fixed listing display

--- 1_queries/3_property_listings_by_city.sql
+++ 1_queries/3_property_listings_by_city.sql
@@ -1,8 +1,8 @@
-SELECT properties.id, title, cost_per_night, avg(property_reviews.rating) as average_rating
+SELECT properties.id, title, properties.cost_per_night, avg(property_reviews.rating) as average_rating
 FROM properties
-LEFT JOIN property_reviews ON properties.id = property_id
+LEFT JOIN property_reviews ON properties.id = property_reviews.property_id
 WHERE city = 'Vancouver'
 GROUP BY properties.id
 HAVING avg(property_reviews.rating) >= 4
-ORDER BY cost_per_night
+ORDER BY cost_per_night ASC
 LIMIT 10;
--- 1_queries/4_most_visited_cities.sql
+++ 1_queries/4_most_visited_cities.sql
@@ -1,5 +1,5 @@
-SELECT city, count(reservations.*) as total_reservations
+SELECT properties.city, count(reservations.*) as total_reservations
 FROM properties
-JOIN reservations ON properties.id = property_id
-GROUP BY city
+JOIN reservations ON properties.id = reservations.property_id
+GROUP BY properties.city
 ORDER BY total_reservations DESC;
