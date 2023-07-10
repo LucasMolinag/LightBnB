3b0218b31323f9a81bc367afef8e342a86996f64
Lucas <molinag.lucas@gmail.com>
2023-07-09 22:41:35 -0700

    Bug fix

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,8 +1,3 @@
-INSERT INTO reservations (start_date, end_date, property_id, guest_id)
-VALUES ('2018-09-11', '2018-09-26', '2', '3'),
-('2019-01-04', '2019-02-01', '2', '2'),
-('2021-10-01', '2021-10-14','1', '3');
-
 INSERT INTO users (name, email, password)
 VALUES ('Eva Stanley', 'sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
 ('Louisa Meyer', 'jacksonrose@hotmail.com', '$2b$10$bomFotjDYEmLMFIslwNdleAQPDHNVQBWL3X4sM528J/ElhHmfK0AG'),
@@ -14,7 +9,12 @@ VALUES ('1', 'Speed lamp', 'description', 'https://images.pexels.com/photos/2086
 ('2', 'Blank corner', 'description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 85234, '6', '6', '7', 'Canada', '651 Nami Road', 'Bohbatev', 'Albert', '83680', TRUE),
 ('3', 'Habit mix', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 46058, '0', '5', '6', 'Canada', '1650 Hejto Center', 'Genwezuj', 'Newfoundland And Labrador', '44583', TRUE);
 
+INSERT INTO reservations (start_date, end_date, property_id, guest_id)
+VALUES ('2018-09-11', '2018-09-26', '1', '3'),
+('2019-01-04', '2019-02-01', '2', '2'),
+('2021-10-01', '2021-10-14','3', '1');
+
 INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
-VALUES ('3', '2', '22', '3', 'messages'),
-('2', '2', '23', '4', 'messages'),
-('3', '1', '24', '4', 'messages');
+VALUES ('3', '1', '1', '4', 'messages'),
+('2', '2', '2', '5', 'messages'),
+('1', '3', '3', '3', 'messages');
